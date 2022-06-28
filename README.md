e2647d22bb5d41ef36297a8644d06d2e7404bdf5
Mitay <>
2022-06-28 18:29:53 +0000

    Templates has added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GoWikiApp
 Learning how to create web apps with Go
+https://golang-blog.blogspot.com/p/go-web-app.html
--- wiki.go
+++ wiki.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
   "io/ioutil"
   //"os"
+  "net/http"
+  "log"
+  "html/template"
 )
 
 type Page struct {
@@ -11,19 +14,51 @@ type Page struct {
   Body []byte
 }
 func (p *Page) Save() error {
+  //TODO to lower case
   filename := p.Title + ".txt"
   return ioutil.WriteFile(filename, p.Body, 0600)
   //return os.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
+  //TODO to lower case
   filename := title + ".txt"
-  //TODO errors
   //body, _ := os.ReadFile(filename)
-  body, _ := ioutil.ReadFile(filename)
-  return &Page{Title: title, Body: body}
+  body, err := ioutil.ReadFile(filename)
+  if err != nil {
+    return nil, err
+  }
+  return &Page{Title: title, Body: body}, nil
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  //http.HandleFunc("/save/", saveHandler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// This function handles URL with prefix "/view/"
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  //TODO to lower case
+  title := r.URL.Path[len("/view/"):]
+  p,_ := loadPage(title)
+  fmt.Fprintf(w,"<h1>%s</h1>"+
+              "<div><h2>%s</h2></div>", p.Title, 
+ p.Body)
+}
+
+// This function allows to edit Pages
+func editHandler( w http.ResponseWriter, r *http.Request){
+  title := r.URL.Path[len("/edit/"):]
+  p, err := loadPage(title)
+  if err != nil {
+    p = &Page{Title: title}
+  }
+  fmt.Fprintf(w, "<h1>Editing <i>%s</i></h1>"+
+             "<form action=\"/save/%s\" method=\"POST\">"+
+             "<textarea name=\"body\">%s</textarea><br>"+
+             "<input type=\"submit\" value=\"Save\">"+
+             "</form>",
+             p.Title, p.Title, p.Body)
 }
